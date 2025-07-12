c0fcb107ea21d5912e8404e4d9949d3a0c935daa
ltkfa <ae86mm@gmail.com>
2025-07-11 21:00:32 -0600

    update

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
- print("hola mundo ")
+
+lista = []
+lista = []
+class Estudiantes:
+    def __init__(self,nombre,carne,carrera,notafinal):
+        self.nombre = nombre
+        self.carne = carne
+        self.carrera = carrera
+        self.notafinal = notafinal
+
+    def MostrarInformacino(self ):
+        print(self.nombre,self.carne,self.carrera,self.notafinal)
+
+    def PromedioTotal(self):
+        print("Promedio es")
+
+
+print("\nRegistrar nuevo estudiante")
+print("Ingrese nombre y apellido")
+nombre = input()
+print("Ingrese carne")
+carne = input()
+print("Ingrese carrera")
+carrera = input()
+print("Ingrese nota final")
+notafinal = input()
+
+
+
+
+
 
